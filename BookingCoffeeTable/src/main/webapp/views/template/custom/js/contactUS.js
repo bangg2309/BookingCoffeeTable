6656afaff0e82e4af47f8dbd213847a79db38e47
NguyenHuuTinh1 <21130202@st.hcmuaf.edu.vn>
2023-10-25 21:11:20 +0700

    up code login - contactUS

---
+++ BookingCoffeeTable/src/main/webapp/views/template/custom/js/contactUS.js
@@ -0,0 +1,23 @@
+<script src="https://maps.google.com/maps/api/js"></script>
+<script type="text/javascript">
+    /* WOW.js init */
+    new WOW().init();
+    // MDB Lightbox Init
+    $(function () {
+    $("#mdb-lightbox-ui").load("https://mdbootstrap.com/previews/templates/e-commerce/mdb-addons/mdb-lightbox-ui.htmll");
+});
+    // Tooltips Initialization
+    $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+    // Material Select Initialization
+    $(document).ready(function () {
+    $('.mdb-select').material_select();
+});
+
+
+    // SideNav Initialization
+    $(".button-collapse").sideNav();
+
+</script>
